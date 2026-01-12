640e90ddf1a89972294fd5a5f4dcd16c86e58e68
neraziim <neraziim@gmail.com>
2026-01-13 08:57:46 +0900

    feat: add custom HttpException class to manage API status codes

---
+++ src/errors/http.Exception.js
@@ -0,0 +1,9 @@
+export class HttpException extends Error {
+  statusCode; //상태코드 프로퍼티
+  //생성자 함수: description, statusCode를 매개변수로 하는 error 객체 생성
+  constructor(description, statusCode) {
+    super(description); //Error.message에 description 전달
+    this.name = this.constructor.name; //생성할 error객체의 name(기본값 Error.name 상속, Error)에 HttpException.name(HttpException) 할당
+    this.statusCode = statusCode; //생성할 error객체의 상태코드 프로퍼티(HttpException.statusCode)에 매개변수로 받은 statusCode 할당
+  }
+}
